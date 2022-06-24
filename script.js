9c98968d6c5444e0ce295574e57058b78463d11d
aimeemisaki <aimeejmisaki@gmail.com>
2022-06-24 15:27:37 -0700

    fixed scroll down hover issue, added css styling to welcome page and added fixed nav bar

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-// header 
-let header = document.querySelector('.header')
+// hiding scroll down text
+let scrollDown = document.querySelector('.scroll-down')
+scrollDown.style.visibility = 'hidden'
+// down arrow to scroll on hover
+let downArrowBox = document.querySelector('.down-img-box')
+let downArrow = document.querySelector('.down-img')
+
+downArrowBox.addEventListener('mouseover', function downMouseOver () {
+    scrollDown.style.visibility = 'visible'
+    downArrow.style.visibility = 'hidden'
+}) 
+
+downArrowBox.addEventListener('mouseout', function downMouseOut () {
+    scrollDown.style.visibility = 'hidden'
+    downArrow.style.visibility = 'visible'
+})
+
+
+// grabbing header
+let header = document.querySelector('header')
 // header offset position 
 let sticky = header.offsetTop
 // functions for sticky header 
-body.onscroll = function() {stickyHeader()};
-
 function stickyHeader () {
-    if (body.pageYOffset > sticky) {
+    if (window.pageYOffset > sticky) {
       header.classList.add("sticky");
     } else {
       header.classList.remove("sticky");
     }
   }
+window.onscroll = function() {stickyHeader()};
+
+
+
