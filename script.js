016d14567042bd2f6ef43d9585958cea89370724
aimeemisaki <aimeejmisaki@gmail.com>
2022-06-26 20:32:09 -0700

    added project, edited nav bar to become transparent onscroll, added font, added css styling for project page

--- script.js
+++ script.js
@@ -15,20 +15,51 @@ downArrowBox.addEventListener('mouseout', function downMouseOut () {
     downArrow.style.visibility = 'visible'
 })
 
+// HEADER (NAV BAR) JS
+
 
 // grabbing header
 let header = document.querySelector('header')
+
 // header offset position 
 let sticky = header.offsetTop
-// functions for sticky header 
+header.style.background = 'transparent'
+
+// grabbing header texts
+let headerTxt = document.querySelector('.header-txt')
+let aimeeTxt = document.querySelector('#aimee')
+
+// function for sticky header // changing header to white on scroll // changing font color 
+// ?? why is header text not changing color
 function stickyHeader () {
     if (window.pageYOffset > sticky) {
-      header.classList.add("sticky");
+      header.classList.add('sticky');
+      header.style.background = 'white';
+      headerTxt.style.color = 'lightgrey';
     } else {
-      header.classList.remove("sticky");
+      header.classList.remove('sticky');
+      header.style.background = 'transparent'
+      headerTxt.style.color = 'grey'
     }
   }
 window.onscroll = function() {stickyHeader()};
+
+// header txt mouseover change color
+// grabbing txt elements
+
+// mouseover / mouseout events 
+// headerTxt.addEventListener('mouseover', function headerTxtMouseover () {
+//     headerTxt.style.color = 
+// })
+
+
+// WELCOME PAGE
+
+// opening new tab for linkedin
+let linkedinBtn = document.querySelector('.linkedin-btn')
+linkedinBtn.addEventListener ('click', function LinkedinLink () {
+    window.open('https://www.linkedin.com/in/aimee-misaki-1a6b5212a/', '_blank');
+})
 
 
 
