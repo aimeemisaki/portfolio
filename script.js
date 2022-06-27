e2e898325d79cda0e8296ceee568751d5dc928fa
aimeemisaki <aimeejmisaki@gmail.com>
2022-06-27 11:08:47 -0700

    added phone number modal (still fixing)

--- script.js
+++ script.js
@@ -61,6 +61,70 @@ linkedinBtn.addEventListener ('click', function LinkedinLink () {
     window.open('https://www.linkedin.com/in/aimee-misaki-1a6b5212a/', '_blank');
 })
 
+// PROJECTS PAGE
+
+//resume btn
+let resumeBtn = document.querySelector('.resume-btn')
+let resumeModal 
+// open resume PDF
+resumeBtn.addEventListener('click', function openResume () {
+    window.open
+})
+
+
+// CONTACT ME PAGE
+
+// setting up email button
+let emailBtn = document.querySelector('#email-btn')
+// open email app event listner
+emailBtn.addEventListener('click', function openEmail () {
+    window.open ('mailto:aimeejmisaki@gmail.com')
+})
+
+// setting up phone button and phone modal
+let phoneBtn = document.querySelector('#phone-btn')
+
+let phoneModal = document.querySelector('.phone-modal-box')
+
+let phoneSubmitBtn = document.querySelector('.phone-submit')
+
+let phoneNum = document.querySelector('.phone-modal-answer')
+
+phoneNum.style.display = 'none'
+
+// before phone click event
+// phoneModal.style.visibility = 'hidden'
+// phone buttone event listener
+phoneBtn.addEventListener('click', function phoneModalPopup () {
+    phoneModal.style.display = 'visible'
+})
+
+//phone modal event listener
+
+let firstNameInput = document.querySelector('#first-name')
+let lastNameInput = document.querySelector('#last-name')
+
+let nameAnswer = document.querySelector('.phone-modal-answer-box')
+
+// phone modal event listeners
+
+phoneSubmitBtn.addEventListener('click', function namesInput () {
+    if (firstNameInput == 'Aimee' && lastNameInput == 'Misaki') {
+        phoneNum.style.display = 'inline-block'
+    } else { 
+        window.alert('Wrong name!')
+    }
+})
+
+
+
+
+
+
+
+
+
+
 
 // FOOTER
 
